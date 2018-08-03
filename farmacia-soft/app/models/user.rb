8b4be729acc2454124ea24a4276c25e08785c127
diego.10989@gmail.com <diego.10989@gmail.com>
2018-08-03 14:57:38 -0400

    Cambios en el modelo de Usuario

--- farmacia-soft/app/models/user.rb
+++ farmacia-soft/app/models/user.rb
@@ -1,7 +1,8 @@
-require 'bcrypt'
+
 class User < ApplicationRecord
   authenticates_with_sorcery!
-  include BCrypt
+
+   has_secure_password validations: false
   # validaciones de usuario
   validates :first_name, presence: true
   validates :last_name, presence: true
@@ -10,12 +11,4 @@ class User < ApplicationRecord
   validates :password,length: { minimum: 8}
   validates :password, confirmation: { case_sensitive: true,:on=>[:new,:edit]}
   
-  def password
-    @password ||= Password.new(crypted_password)
-  end
-
-  def password=(new_password)
-    @password = Password.create(new_password)
-    self.crypted_password = @password
-  end
 end
